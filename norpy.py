e8ee38a120f3f882dd548cd29de322bbebf0a4cf
mo2561057 <moritz.mendel12@gmail.com>
2019-08-14 18:02:03 +0200

    Performing Monte Carlo Comaprisons. Base camp is built

---
+++ norpy.py
@@ -0,0 +1,29 @@
+"""
+Simualte the norpy model with respy package under previously estimated
+optimal paramters.
+Use this as a sanity check of the model correctness.
+"""
+import yaml
+
+import respy as rp
+import numpy as np
+import pandas as pd
+
+from ov_respy_config import TEST_RESOURCES_DIR
+from validation_utils import get_moments
+
+#Get the basic modelstructure
+options = yaml.safe_load((TEST_RESOURCES_DIR / f"norpy_estimates.yaml").read_text())
+params = pd.read_csv(
+        TEST_RESOURCES_DIR / f"norpy_estimates.csv", index_col=["category", "name"]
+    )
+
+#Get rid of weird string values
+for x in params["value"]:
+    x = float(x)
+
+#Simulate the data with  the specified coefficeints
+simulate = rp.get_simulate_func(params, options)
+df = simulate(params)
+moments = get_moments(df)
+
---
+++ ov_respy_config.py
@@ -0,0 +1,12 @@
+"""
+This file contains configuration for this repo.
+"""
+"""General configuration for respy."""
+from pathlib import Path
+
+# Obtain the root directory of the package. Do not import respy which creates a circular
+# import.
+ROOT_DIR = Path(__file__).parent
+
+# Directory with additional resources for the testing harness
+TEST_RESOURCES_DIR = ROOT_DIR / "ressources"
